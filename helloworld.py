21341e0254f69c8c06ea5245f4906d61b266e415
rohit <rohit@gmail.com>
2018-12-12 15:49:04 +0530

    Hello world completed

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,6 @@
-wish=input("Enter you wish:")
-print("Hello {}".format(wish))
+def wishes(wish):
+    print("Hello {}".format(wish))
+if __name__=='__main__':
+    wish=input("Enter you wish:")
+    wishes(wish)
+    print("---Completd-----")
